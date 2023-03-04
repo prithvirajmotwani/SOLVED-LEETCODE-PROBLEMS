f275f021c6bb37380d723d2f0ec65efb5398e237
Prithvi Raj <100158091+prithvirajmotwani@users.noreply.github.com>
2023-03-04 23:54:19 +0500

    Type: Easy

---
+++ Q11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]!= val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+
+
+int main() {
+
+  return 0;
+}
